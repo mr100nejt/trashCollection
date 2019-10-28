fd4db5845f71a258789e30a16ba0898a3177a37a
jacob thompson <you@example.com>
2019-10-28 14:52:36 -0500

    intial setup/customer/employee

---
+++ Models/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollection.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int zipCode { get; set; }
+        public string streetAdress { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public int balance { get; set; }
+        public int monthleyCharge { get; set; }
+        public bool pickupConfirmation { get; set; }
+        public string startDateForNoPickup { get; set; }
+        public string endDateForNoPickup { get; set; }
+
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TrashCollection.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public int zipCode { get; set; }
+    }
+}
